7840b987eaf2ec20c3518f23b51d68b9dfde5a33
kanjeranian <oilscius@gmail.com>
2019-05-31 12:44:27 +0700

    update UI

--- quest4/script.js
+++ quest4/script.js
@@ -16,6 +16,7 @@ function getAllUsers(){
 
           var eachMember = document.createElement('div');
           eachMember.className='eachMember';
+          eachMember.id='member'+id;
 
           whiteBox = document.createElement('div');
           whiteBox.className='whiteBox';
@@ -26,7 +27,6 @@ function getAllUsers(){
           img = document.createElement('img');
           img.id='a'+id;
           img.src=image;
-          img.onclick=function(){editUser2( (this.id).substring(1,) )}; /////////////
 
           h2 = document.createElement('h2');
           h2.innerHTML = name+" "+surname+"<br>("+gender+")";
@@ -36,10 +36,20 @@ function getAllUsers(){
           a = document.createElement('a');
           a.id='a'+id;
           a.href='#deleteUser';
-          a.onclick=function(){deleteUser( (this.id).substring(1,) )}; /////////////
+          a.onclick=function(){editUser2( (this.id).substring(1,) )}; /////////////
 
-          bin = document.createElement('img');
-          bin.src = '../img/greyBin.svg';
+          editIcon = document.createElement('img');
+          editIcon.src = '../img/editIcon.svg';
+          editIcon.className='editIcon'; /////////////
+
+          a2 = document.createElement('a');
+          a2.id='a2'+id;
+          a2.href='#deleteUser';
+          a2.onclick=function(){deleteUser( (this.id).substring(2,) )}; /////////////
+
+          deleteIcon = document.createElement('img');
+          deleteIcon.src = '../img/deleteIcon.svg';
+          deleteIcon.className='deleteIcon'
 
           h7 = document.createElement('h7');
           h7.innerHTML = 'delete user';
@@ -49,37 +59,16 @@ function getAllUsers(){
           whiteBox.appendChild(img);
           whiteBox.appendChild(h2);
           
-          a.appendChild(bin);
-          a.appendChild(h7);
+          a.appendChild(editIcon);
+          a2.appendChild(deleteIcon); ///////
 
           eachMember.appendChild(whiteBox);
           eachMember.appendChild(br);
           eachMember.appendChild(a);
+          eachMember.appendChild(a2);
   
           document.getElementById("show").appendChild(eachMember);
           
-
-          // <div class="eachMember">
-          //   <div class="whiteBox">
-          //     <h1>id) nickname</h1>
-          //     <img src="img"> </img>
-          //     <h2>name+" "+surname</br>(gender)</h2>
-          //   </div>
-          //   </br>
-          //   <a href="#deleteUser"> <img src="../img/greyBin.svg"> </img> <h7>delete user</h7> </a>
-          // </div>
-
-
-
-          // <div class="eachMember">
-          //   <div class="whiteBox">
-          //     <h1>id) nickname</h1>
-          //     <img src="img"> </img>
-          //     <h2>name+" "+surname</br>(gender)</h2>
-          //   </div>
-          //   </br>
-          //   <a href="#deleteUser"> <img src="../img/greyBin.svg"> </img> <h7>delete user</h7> </a>
-          // </div>
       } 
       if(n==0){
         eachMember+='<div class=divDog> <img id="dogIMG" src="img/dog.svg"> </img> </div>';
@@ -90,42 +79,41 @@ function getAllUsers(){
     })
   }
 
-  function createUser(){
-    hideForm();
-    var name    = document.getElementById("createUserForm").elements.namedItem("name").value;
-    var surname = document.getElementById("createUserForm").elements.namedItem("surname").value;
-    var nickname = document.getElementById("createUserForm").elements.namedItem("nickname").value;
-    var gender = document.getElementById("createUserForm").elements.namedItem("gender").value;
-    var image = document.getElementById("createUserForm").elements.namedItem("image").value;
-    var text = "name="+name+"&surname="+surname+"&nickname="+nickname+"&gender="+gender+"&image="+image;
-    var xmlhttp = new XMLHttpRequest();
-    var str="";
+function createUser(){
+  hideForm();
+  var name    = document.getElementById("createUserForm").elements.namedItem("name").value;
+  var surname = document.getElementById("createUserForm").elements.namedItem("surname").value;
+  var nickname = document.getElementById("createUserForm").elements.namedItem("nickname").value;
+  var gender = document.getElementById("createUserForm").elements.namedItem("gender").value;
+  var image = document.getElementById("createUserForm").elements.namedItem("image").value;
+  var text = "name="+name+"&surname="+surname+"&nickname="+nickname+"&gender="+gender+"&image="+image;
+  var xmlhttp = new XMLHttpRequest();
+  var str="";
 
-    axios.post('https://sb-oil-web-bootcamp.herokuapp.com/users', {
-      name: name,
-      surname: surname,
-      nickname: nickname,
-      gender: gender,
-      image: image
-    })
-    .then(function (response) {
-      console.log(response);
-      var obj = response.data;
-      var status = obj.status;
-      if(status==0){
-        errMessage=obj.error;
-        document.getElementById("createUserForm").reset();
-        alert(errMessage);
-        unhideForm();
-      }else{
-        getAllUsers();
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-
+  axios.post('https://sb-oil-web-bootcamp.herokuapp.com/users', {
+    name: name,
+    surname: surname,
+    nickname: nickname,
+    gender: gender,
+    image: image
+  })
+  .then(function (response) {
+    console.log(response);
+    var obj = response.data;
+    var status = obj.status;
+    if(status==0){
+      errMessage=obj.error;
+      document.getElementById("createUserForm").reset();
+      alert(errMessage);
+      unhideForm();
+    }else{
+      getAllUsers();
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
 function editUser2(id){
   //document.getElementById('EDIT_USER').style.color = "#D66A6A";
   formBox = document.createElement('div');
@@ -227,9 +215,6 @@ function editUser2(id){
   document.getElementById("show").innerHTML = "";
   document.getElementById("show").appendChild(formBox);
 }
-
-
-
 function editUser(){
   hideForm();
   var id        = document.getElementById("editUserForm").elements.namedItem("id").value;
@@ -264,7 +249,6 @@ function editUser(){
   });
 
 }
-
 function deleteUser(){
   hideForm();
   var id        = document.getElementById("userForm").elements.namedItem("id").value;
@@ -287,7 +271,6 @@ function deleteUser(){
   })
 
 }
-
 function deleteUser(id){
   axios.delete('https://sb-oil-web-bootcamp.herokuapp.com/users/'+id)
   .then(function (response) {
@@ -307,12 +290,9 @@ function deleteUser(id){
   })
 
 }
-
 function hideForm(){
   document.getElementById("hiddenForm").style.visibility = "hidden";
 }
-
 function unhideForm(){
   document.getElementById("hiddenForm").style.visibility = "visible";
 }
-
